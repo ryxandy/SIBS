744638365d2220ad9a22350cba891ed8e53a8564
Alexandre Lopes <ryxandy@hotmail.com>
2024-06-05 14:44:50 -0300

    creating te orderController and a few endpoints

---
+++ src/main/java/com/cocus/manage/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.cocus.manage.controller;
+
+import com.cocus.manage.model.Order;
+import com.cocus.manage.repository.OrderRepository;
+import com.cocus.manage.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping
+    public List<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+        Optional<Order> order = orderRepository.findById(id);
+        if (order.isPresent()) {
+            return ResponseEntity.ok().body(order.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public Order createOrder(@RequestBody Order order) {
+        return orderService.createOrder(order);
+    }
+    
+}
