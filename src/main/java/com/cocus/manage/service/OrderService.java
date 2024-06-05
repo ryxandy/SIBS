a07ebcf8cc3c8103c9236797af55f7e3f74b7c43
Alexandre Lopes <ryxandy@hotmail.com>
2024-06-05 13:50:47 -0300

    configuring email sender to send email when an order is completed

--- src/main/java/com/cocus/manage/service/OrderService.java
+++ src/main/java/com/cocus/manage/service/OrderService.java
@@ -6,6 +6,7 @@ import com.cocus.manage.repository.OrderRepository;
 import com.cocus.manage.repository.StockMovementRepository;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -53,6 +54,13 @@ public class OrderService {
 
         //need to come back here to create the create update the stock method
         logOrderCompletion(order);
+    }
+
+    private void sendOrderCompletionEmail(Order order) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(order.getUser().getEmail());
+        message.setSubject("Order Completed");
+        message.setText("Your order for " + order.getQuantity() + " " + order.getItem().getName() + " is complete.");
     }
 
     private void logOrderCompletion(Order order) {
