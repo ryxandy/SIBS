adfd82299a30fe9d0a2acfa75306e0f93b68ab63
Alexandre Lopes <ryxandy@hotmail.com>
2024-06-05 17:32:34 -0300

    updating the way that i'm sending the email to user

--- src/main/java/com/cocus/manage/mail/MailConfig.java
+++ src/main/java/com/cocus/manage/mail/MailConfig.java
@@ -1,5 +1,6 @@
 package com.cocus.manage.mail;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,10 +14,10 @@ public class MailConfig {
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.example.com");
+        mailSender.setHost("mula150ml@gmail.com");
         mailSender.setPort(587);
         mailSender.setUsername("xandiynho@hotmail.com");
-        mailSender.setPassword("skatedoido");
+        mailSender.setPassword("88719588a");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
